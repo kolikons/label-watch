0ebd0ff827d529cc8fda12528bf50fee3216ae72
kolikons <kolikons@gmail.com>
2021-03-13 14:04:58 +0200

    First configuration of application

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/kolikons/label-watch/cmd"
+	"github.com/kolikons/label-watch/pkg/label"
+)
+
 func main() {
-	// label.add()
+	opts, e := cmd.ParseFlags()
+	if e != nil {
+		fmt.Print(e)
+		os.Exit(0)
+	}
+	fmt.Println("Running label-watch")
+	// Run Lable pkg to set labels
+	if opts.Interval == "" {
+		label.RunLabel(opts)
+	}
+
+	// Run Ticker daemon
+	if opts.Interval != "" {
+		label.RunTimerLabel(opts)
+	}
+
+	os.Exit(0)
 }
